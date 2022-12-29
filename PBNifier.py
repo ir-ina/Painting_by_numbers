c2ff12e2bed4569067d20b331a4e4b72b4c7cc94
Irina Evmenenkova <irinaevmenenkova@gmail.com>
2022-12-29 20:32:38 +0300

    initial commit

--- PBNifier.py
+++ PBNifier.py
@@ -1,23 +1,30 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 from scipy.spatial import KDTree
 from webcolors import CSS3_HEX_TO_NAMES, hex_to_rgb
 import os.path
 
 
-class Painting():
+class Painting:
     """
-    Converting any image to a Painting By Numbers.
-    The final product is the color segmented image, the outlined image for coloring and the palette.
-
+    Convert any image to a Painting By Numbers.
+    The result is the segmented by colors image, the outlined image for coloring and the palette.
+    Parameters:
+    img_path: path-like object
+        path to the image to transform into a Painting
+    nb_colors: integer, optional, default 10
+        number of colors to keep
+    pixel_size: integer, optional, default 4000
+        size in pxl of the largest dimension of the ouptut canvas
+    save : boolean, optional, default True
+        whether to save results or not
     """
 
-    def __init__(self, path_pic, nb_color=10, pixel_size=4000, save=True):
-        self.namefile = os.path.basename(path_pic).split(".")[0]
-        self.dirpath = os.path.dirname(path_pic)
-        self.src = cv2.cvtColor(cv2.imread(path_pic), cv2.COLOR_BGR2RGB)
-        self.nb_color = nb_color
+    def __init__(self, img_path, nb_colors=10, pixel_size=4000, save=True):
+        self.namefile = os.path.basename(img_path).split(".")[0]
+        self.dirpath = os.path.dirname(img_path)
+        self.src = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+        self.nb_color = nb_colors
         self.tar_width = pixel_size
         self.save = save
         self.colormap = []
@@ -27,31 +34,36 @@ class Painting():
         resized_img = self.resize()
         clean_img = self.cleaning(resized_img)
         segmented_image, colors = self.segmentation(clean_img)
-        canvas = np.ones(segmented_image.shape[:3], dtype="uint8") * 255
+        # create empty canvas
+        canvas = np.full(segmented_image.shape, 255, dtype="uint8")
 
         for ind, color in enumerate(colors):
             self.colormap.append([int(c) for c in color])
+            # find contours for every segment of every main color
             mask = cv2.inRange(segmented_image, color, color)
             cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
             cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
             for contour in cnts:
+                # draw contours of appropriate size
                 _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
                 if width_ctr > 20 and height_ctr > 20 and cv2.contourArea(contour, True) < -100:
                     cv2.drawContours(canvas, [contour], -1, 0, 1)
-                    # Add label
-                    txt_x, txt_y = contour[0][0]
-                    cv2.putText(canvas, '{:d}'.format(ind + 1),
-                                (txt_x, txt_y + 15),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 1)
+                    # add color label to the contour centre
+                    M = cv2.moments(contour)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    cv2.putText(canvas, '{:d}'.format(ind + 1), (cX, cY + 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 1)
 
         palette = self.display_colormap()
 
         if self.save:
             cv2.imwrite(os.path.join(self.dirpath, f"{self.namefile}-colored.png"),
                         cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB))
-            cv2.imwrite(os.path.join(self.dirpath, f"{self.namefile}-canvas.png"), cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB))
-            cv2.imwrite(os.path.join(self.dirpath, f"{self.namefile}-palette.png"), cv2.cvtColor(palette, cv2.COLOR_BGR2RGB))
+            cv2.imwrite(os.path.join(self.dirpath, f"{self.namefile}-canvas.png"),
+                        cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB))
+            cv2.imwrite(os.path.join(self.dirpath, f"{self.namefile}-palette.png"),
+                        cv2.cvtColor(palette, cv2.COLOR_BGR2RGB))
 
         return segmented_image, canvas, palette
 
@@ -65,11 +77,12 @@ class Painting():
         return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
 
     def cleaning(self, picture):
-        """Reduction of noize, morphological transformations """
+        """Reducе noize, morphological transformations, blur """
         clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
         clean_pic = cv2.morphologyEx(clean_pic, cv2.MORPH_OPEN, kernel, cv2.BORDER_REPLICATE)
         clean_pic = cv2.morphologyEx(clean_pic, cv2.MORPH_CLOSE, kernel, cv2.BORDER_REPLICATE)
+        clean_pic = cv2.medianBlur(clean_pic, 5)
         return clean_pic
 
     def segmentation(self, picture):
@@ -80,12 +93,14 @@ class Painting():
         ret, label, center = cv2.kmeans(vectorized, self.nb_color, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
         center = np.uint8(center)
         res = center[label.flatten()]
-        result_image = res.reshape((picture.shape))
+        result_image = res.reshape(picture.shape)
         return result_image, center
 
     def display_colormap(self):
         """Create palette as a picture for the user"""
-        picture = np.ones((len(self.colormap) * 60 + 40, 600, 3), dtype="uint8") * 255
+        # create empty palette image
+        picture = np.full((len(self.colormap) * 60 + 40, 600, 3), 255, dtype="uint8")
+        # add every main color to the palette image
         for ind, col in enumerate(self.colormap):
             cv2.putText(picture, '{:d}'.format(ind + 1), (20, 60 * ind + 46), cv2.FONT_HERSHEY_SIMPLEX, 1.2, 0, 2)
             cv2.rectangle(picture, (90, 60 * ind + 10), (170, 60 * ind + 50), col, thickness=-1)
@@ -105,4 +120,9 @@ class Painting():
         distance, index = kdt_db.query(rgb)
         return names[index]
 
-
+    def get_paths(self):
+        """Get paths where the result images are saved"""
+        canvas_path = os.path.join(self.dirpath, f"{self.namefile}-canvas.png")
+        colored_path = os.path.join(self.dirpath, f"{self.namefile}-colored.png")
+        palette_path = os.path.join(self.dirpath, f"{self.namefile}-palette.png")
+        return canvas_path, colored_path, palette_path
